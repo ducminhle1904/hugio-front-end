934f5cfe79cad95c1e557e76c52f83cff91a8f3a
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-08-20 00:27:39 +0700

    update helm and deploy

--- run-deploy.sh
+++ run-deploy.sh
@@ -64,28 +64,28 @@ echo '>>>>>>>>>>>>>> Push order image'
 kind load docker-image $appOrderDockerImage:$dockerTag
 
 echo '>>>>>>>>>>>>>> Deploy shell image'
-helm upgrade -i --set image.name=$appShellDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4200 -n $k8sNamespace app-shell ./deploy/shell/helm_chart
+helm upgrade -i --set image.name=$appShellDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4200 -n $k8sNamespace app-shell ./deploy/shell/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy analysis image'
-helm upgrade -i --set image.name=$appAnalysisDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4202 -n $k8sNamespace app-analysis ./deploy/analysis/helm_chart
+helm upgrade -i --set image.name=$appAnalysisDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4202 -n $k8sNamespace app-analysis ./deploy/analysis/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy auth image'
-helm upgrade -i --set image.name=$appAuthDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4206 -n $k8sNamespace app-auth ./deploy/auth/helm_chart
+helm upgrade -i --set image.name=$appAuthDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4206 -n $k8sNamespace app-auth ./deploy/auth/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy cashbook image'
-helm upgrade -i --set image.name=$appCashbookDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4203 -n $k8sNamespace app-cashbook ./deploy/cashbook/helm_chart
+helm upgrade -i --set image.name=$appCashbookDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4203 -n $k8sNamespace app-cashbook ./deploy/cashbook/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy product image'
-helm upgrade -i --set image.name=$appProductDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4205 -n $k8sNamespace app-product ./deploy/product/helm_chart
+helm upgrade -i --set image.name=$appProductDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4205 -n $k8sNamespace app-product ./deploy/product/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy summary image'
-helm upgrade -i --set image.name=$appSummaryDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4201 -n $k8sNamespace app-summary ./deploy/summary/helm_chart
+helm upgrade -i --set image.name=$appSummaryDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4201 -n $k8sNamespace app-summary ./deploy/summary/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy user image'
-helm upgrade -i --set image.name=$appUserDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4204 -n $k8sNamespace app-user ./deploy/user/helm_chart
+helm upgrade -i --set image.name=$appUserDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4204 -n $k8sNamespace app-user ./deploy/user/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy order image'
-helm upgrade -i --set image.name=$appOrderDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_target_port=4207 -n $k8sNamespace app-order ./deploy/order/helm_chart
+helm upgrade -i --set image.name=$appOrderDockerImage,image.tag=$dockerTag,replica=$k8sReplica,tcp_port=4207 -n $k8sNamespace app-order ./deploy/order/helm_chart
 
 echo '>>>>>>>>>>>>>> Clean image'
 docker rmi $appShellDockerImage:$dockerTag
