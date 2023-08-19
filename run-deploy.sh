ecba3c9c3a353bd496ab7867c2ebe19547ebcbe4
Võ Tuấn Lộc <tuanloc1105@gmail.com>
2023-08-19 23:47:55 +0700

    update deploy script

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 dockerTag=$(date -d "$b 0 min" "+%Y%m%d%H%M%S")
-appShellDockerImage='hugio_app_shell'
-appAnalysisDockerImage='hugio_app_analysis'
-appAuthDockerImage='hugio_app_auth'
-appCashbookDockerImage='hugio_app_cashbook'
-appProductDockerImage='hugio_app_product'
-appSummaryDockerImage='hugio_app_summary'
-appUserDockerImage='hugio_app_user'
-appOrderDockerImage='hugio_app_order'
+appShellDockerImage='hugio/app_shell'
+appAnalysisDockerImage='hugio/app_analysis'
+appAuthDockerImage='hugio/app_auth'
+appCashbookDockerImage='hugio/app_cashbook'
+appProductDockerImage='hugio/app_product'
+appSummaryDockerImage='hugio/app_summary'
+appUserDockerImage='hugio/app_user'
+appOrderDockerImage='hugio/app_order'
+k8sNamespace='frontend'
 k8sReplica=1
 
 echo '>>>>>>>>>>>>>> Build source'
@@ -63,28 +64,28 @@ echo '>>>>>>>>>>>>>> Push order image'
 kind load docker-image $appOrderDockerImage:$dockerTag
 
 echo '>>>>>>>>>>>>>> Deploy shell image'
-helm upgrade -i --set image.name=$appShellDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appShellDockerImage ./deploy/shell/helm_chart
+helm upgrade -i --set image.name=$appShellDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_shell ./deploy/shell/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy analysis image'
-helm upgrade -i --set image.name=$appAnalysisDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appAnalysisDockerImage ./deploy/analysis/helm_chart
+helm upgrade -i --set image.name=$appAnalysisDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_analysis ./deploy/analysis/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy auth image'
-helm upgrade -i --set image.name=$appAuthDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appAuthDockerImage ./deploy/auth/helm_chart
+helm upgrade -i --set image.name=$appAuthDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_auth ./deploy/auth/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy cashbook image'
-helm upgrade -i --set image.name=$appCashbookDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appCashbookDockerImage ./deploy/cashbook/helm_chart
+helm upgrade -i --set image.name=$appCashbookDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_cashbook ./deploy/cashbook/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy product image'
-helm upgrade -i --set image.name=$appProductDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appProductDockerImage ./deploy/product/helm_chart
+helm upgrade -i --set image.name=$appProductDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_product ./deploy/product/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy summary image'
-helm upgrade -i --set image.name=$appSummaryDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appSummaryDockerImage ./deploy/summary/helm_chart
+helm upgrade -i --set image.name=$appSummaryDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_summary ./deploy/summary/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy user image'
-helm upgrade -i --set image.name=$appUserDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appUserDockerImage ./deploy/user/helm_chart
+helm upgrade -i --set image.name=$appUserDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_user ./deploy/user/helm_chart
 
 echo '>>>>>>>>>>>>>> Deploy order image'
-helm upgrade -i --set image.name=$appOrderDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace $appOrderDockerImage ./deploy/order/helm_chart
+helm upgrade -i --set image.name=$appOrderDockerImage,image.tag=$dockerTag,replica=$k8sReplica -n $k8sNamespace app_order ./deploy/order/helm_chart
 
 echo '>>>>>>>>>>>>>> Clean image'
 docker rmi $appShellDockerImage:$dockerTag
